96e41ae2f4171b9f00ee1f3604df315cf1ee5b65
cxyxiaoyuyu.com <923996381@qq.com>
2021-12-20 14:52:00 +0800

    传输数据与发送post请求

--- server.js
+++ server.js
@@ -8,5 +8,6 @@ app.all('*',function(req,res,next){
 })
 
 app.get('/data', (req, res) => res.send('Hello World!'))
+app.post('/data', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
