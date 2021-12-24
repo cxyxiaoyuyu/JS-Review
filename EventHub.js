8225a83efb56795b7d5b8d4c5ea005f41399f3a6
cxyxiaoyuyu.com <923996381@qq.com>
2021-12-24 23:31:51 +0800

    eventHub

---
+++ EventHub.js
@@ -0,0 +1,49 @@
+function EventHub() {
+  const callbacks = {}
+  this.on = function (eventName, fn) {
+    callbacks[eventName] = callbacks[eventName] || []
+    callbacks[eventName].push(fn)
+  }
+  this.once = function (eventName,fn) {
+    callbacks[eventName] = callbacks[eventName] || []
+    callbacks[eventName].push(fn)
+    fn.once = true
+  }
+  this.emit = function (eventName, param) {
+    const fnList = callbacks[eventName]
+    if (fnList && fnList.length) {
+      for (let i = 0; i < fnList.length; i++) {
+        const fn = fnList[i]
+        fn(param)
+        if(fn.once){
+          this.off(eventName,fn) 
+        }
+      }
+    }
+  }
+  this.off = function (eventName,fn) {
+    const fnList = callbacks[eventName]
+    if(fnList){
+      const index = fnList.indexOf(fn)
+      if(index > -1){
+        fnList.splice(index,1) 
+      }
+    }
+  }
+}
+
+const bus = new EventHub()
+
+// bus.on('click', function (name) { console.log('click ' + name) })
+// bus.emit('click','catalina')
+
+// const fn = function(){ console.log('该事件将被取消')}
+// bus.on('aaa',fn)
+// bus.emit('aaa')
+// bus.off('aaa',fn)
+// bus.emit('aaa')
+
+bus.once('bbb',function(){console.log('该事件只执行一次')})
+bus.emit('bbb')
+bus.emit('bbb')
+bus.emit('bbb')
