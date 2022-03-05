3b035c7946dbd0f3b7302d691e720122f1409f66
cxyxiaoyuyu.com <923996381@qq.com>
2022-03-05 18:24:22 +0800

    完成promise.allSelttled

--- promise/promise-all.js
+++ promise/promise-all.js
@@ -8,6 +8,7 @@ Promise.myAll = (promises) => {
     }
     promises.forEach((p, i) => {
       p.then((res) => {
+        console.log(i)
         result[i] = res;
         count++;
         if (count === len) {
@@ -27,19 +28,19 @@ const p3 = new Promise((resolve) => {
   setTimeout(() => resolve(3), 3000);
 });
 
-const p4 = new Promise((_, reject) => {
-  setTimeout(() => reject("err4"));
-});
+// const p4 = new Promise((_, reject) => {
+//   setTimeout(() => reject("err4"));
+// });
 // const p5 = Promise.reject("err5");
 // 1. 所有的Promise都成功了
 const p11 = Promise.myAll([p1, p2, p3]).then(console.log).catch(console.log);
 
 // 2. 有一个Promise失败了
-const p12 = Promise.myAll([ p1, p2, p4 ])
-	.then(console.log)
-      .catch(console.log) // err4
+// const p12 = Promise.myAll([ p1, p2, p4 ])
+// 	.then(console.log)
+//       .catch(console.log) // err4
 
 // 3. 有两个Promise失败了，可以看到最终输出的是err4，第一个失败的返回值
-const p13 = Promise.myAll([ p1, p4, Promise.reject('123')])
-	.then(console.log)
-      .catch(console.log) // 123
+// const p13 = Promise.myAll([ p1, p4, Promise.reject('123')])
+// 	.then(console.log)
+//       .catch(console.log) // 123
