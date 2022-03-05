97768db79eaf11dbf88fd18b911c454f17548687
cxyxiaoyuyu.com <923996381@qq.com>
2022-03-05 14:52:34 +0800

    链式调用

--- promise/Promise.ts
+++ promise/Promise.ts
@@ -1,16 +1,16 @@
 class Promise2 {
   // succeed: any = null
   // fail: any = null
-  callbacks:Array<Array<any>> = []
+  callbacks: Array<Array<any>> = []
   state = 'pending'
 
   resolve(result: any) {
     if (this.state !== 'pending') return
     this.state = 'fulfilled'
     setTimeout(() => {
-      this.callbacks.forEach((handle:Array<Array<any>>)=>{
-        if(typeof handle[0] === 'function') {
-          (handle[0] as Function).call(undefined,result) 
+      this.callbacks.forEach((handle: Array<Array<any>>) => {
+        if (typeof handle[0] === 'function') {
+          (handle[0] as Function).call(undefined, result)
         }
       })
     })
@@ -19,9 +19,9 @@ class Promise2 {
     if (this.state !== 'pending') return
     this.state = 'rejected'
     setTimeout(() => {
-      this.callbacks.forEach((handle:Array<Array<any>>)=>{
-        if(typeof handle[1] === 'function') {
-          (handle[1] as Function).call(undefined,reason) 
+      this.callbacks.forEach((handle: Array<Array<any>>) => {
+        if (typeof handle[1] === 'function') {
+          (handle[1] as Function).call(undefined, reason)
         }
       })
     })
@@ -33,14 +33,31 @@ class Promise2 {
     fn(this.resolve.bind(this), this.reject.bind(this))
   }
   then(succeed: any, fail?: any) {
-    const handle:Array<any> = []
+    const handle: Array<any> = []
     if (typeof succeed === 'function') {
       handle[0] = succeed
     }
     if (typeof fail === 'function') {
       handle[1] = fail
     }
+    handle[2] = new Promise2(() => { })
     this.callbacks.push(handle)
+
+    return new Promise2(() => { })
+  }
+  resolveWith(x: any) {
+    if (x instanceof Promise2) {
+      x.then(
+        (result: any) => {
+          this.resolve(result)
+        }, // setTimeout(()=>{})
+        (reason: any) => {
+          this.reject(reason)
+        }
+      )
+    }else{
+      this.resolve(x) 
+    }
   }
 }
 
--- promise/test.ts
+++ promise/test.ts
@@ -7,137 +7,148 @@ import sinonChai from 'sinon-chai'
 
 chai.use(sinonChai)
 
-describe("promise",()=>{
-  it("是一个类",()=>{
+describe("promise", () => {
+  it("是一个类", () => {
     assert.isFunction(Promise2)
     assert.isObject(Promise2.prototype)
   })
-  it("必须接受一个函数",()=>{
-    assert.throw(()=>{
+  it("必须接受一个函数", () => {
+    assert.throw(() => {
       //@ts-ignore
-      new Promise2() 
-    }) 
+      new Promise2()
+    })
   })
-  it('接收的函数立即执行',()=>{
+  it('接收的函数立即执行', () => {
     const fn = sinon.fake()
     const promise = new Promise2(fn)
     assert(fn.called)
   })
-  it('函数有两个函数resolve,reject',()=>{
-    const promise = new Promise2((resolve:any,reject:any)=>{
-      assert.isFunction(resolve) 
-      assert.isFunction(reject) 
+  it('函数有两个函数resolve,reject', () => {
+    const promise = new Promise2((resolve: any, reject: any) => {
+      assert.isFunction(resolve)
+      assert.isFunction(reject)
     })
   })
-  it("返回的实例有then方法",()=>{
-    const promise = new Promise2(()=>{})
+  it("返回的实例有then方法", () => {
+    const promise = new Promise2(() => { })
     assert.isFunction(promise.then)
   })
-  it("then(success)中的success 会在resolve被调用的时候执行",(done)=>{
+  it("then(success)中的success 会在resolve被调用的时候执行", (done) => {
     const success = sinon.fake()
-    const promise = new Promise2((resolve:Function,reject:Function)=>{
+    const promise = new Promise2((resolve: Function, reject: Function) => {
       assert.isFalse(success.called)
-      resolve() 
-      setTimeout(()=>{
+      resolve()
+      setTimeout(() => {
         assert(success.called)
         done()
       })
-    }) 
-    promise.then(success,()=>{})
+    })
+    promise.then(success, () => { })
   })
-  it("then(null,fail)中的fail 会在reject被调用的时候执行",(done)=>{
+  it("then(null,fail)中的fail 会在reject被调用的时候执行", (done) => {
     const fail = sinon.fake()
-    const promise = new Promise2((resolve:Function,reject:Function)=>{
+    const promise = new Promise2((resolve: Function, reject: Function) => {
       assert.isFalse(fail.called)
-      reject() 
-      setTimeout(()=>{
+      reject()
+      setTimeout(() => {
         assert(fail.called)
         done()
       })
-    }) 
-    promise.then(null,fail)
+    })
+    promise.then(null, fail)
   })
-  it("then(x,y) x y 不是函数也不报错",()=>{
-    const promise = new Promise2((resolve:Function) => {
-       resolve()
-    }) 
-    promise.then(false,null) 
+  it("then(x,y) x y 不是函数也不报错", () => {
+    const promise = new Promise2((resolve: Function) => {
+      resolve()
+    })
+    promise.then(false, null)
   })
-  it("then(x,y) x 和 success 的参数一致",(done)=>{
+  it("then(x,y) x 和 success 的参数一致", (done) => {
     const success = sinon.fake()
-    const promise = new Promise2((resolve:Function,reject:Function)=>{
+    const promise = new Promise2((resolve: Function, reject: Function) => {
       assert.isFalse(success.called)
-      resolve(123) 
-      setTimeout(()=>{
+      resolve(123)
+      setTimeout(() => {
         assert(success.called)
         assert(success.calledWith(123))
         done()
       })
-    }) 
-    promise.then(success,()=>{})
+    })
+    promise.then(success, () => { })
   })
-  it("then(x,y) y 和 fail 的参数一致",(done)=>{
+  it("then(x,y) y 和 fail 的参数一致", (done) => {
     const fail = sinon.fake()
-    const promise = new Promise2((resolve:Function,reject:Function)=>{
+    const promise = new Promise2((resolve: Function, reject: Function) => {
       assert.isFalse(fail.called)
-      reject(123) 
-      setTimeout(()=>{
+      reject(123)
+      setTimeout(() => {
         assert(fail.called)
         assert(fail.calledWith(123))
         done()
       })
-    }) 
-    promise.then(null,fail)
+    })
+    promise.then(null, fail)
   })
-  it("succeed 只调用一次",(done)=>{
-    const succeed = sinon.fake() 
-    const promise = new Promise2((resolve:Function)=>{
-      resolve() 
+  it("succeed 只调用一次", (done) => {
+    const succeed = sinon.fake()
+    const promise = new Promise2((resolve: Function) => {
       resolve()
-      setTimeout(()=>{
-        assert(succeed.calledOnce) 
+      resolve()
+      setTimeout(() => {
+        assert(succeed.calledOnce)
         done()
       })
     })
-    promise.then(succeed,null)
+    promise.then(succeed, null)
   })
-  it("在自己代码执行完之前 不得调用succeed",(done)=>{
+  it("在自己代码执行完之前 不得调用succeed", (done) => {
     const succeed = sinon.fake()
-    const promise = new Promise2((resolve:Function)=>{
+    const promise = new Promise2((resolve: Function) => {
       resolve()
       console.log(1)
     })
-    promise.then(succeed,null)
+    promise.then(succeed, null)
     assert.isFalse(succeed.called)
-    setTimeout(()=>{
-      assert(succeed.called) 
-      done() 
+    setTimeout(() => {
+      assert(succeed.called)
+      done()
     })
   })
-  it("succeed 的 this 是undefined",()=>{
-    const promise = new Promise2((resolve:Function)=>{
-      resolve() 
-    }) 
-    promise.then(function(){  // 这里不能用箭头函数测试
+  it("succeed 的 this 是undefined", () => {
+    const promise = new Promise2((resolve: Function) => {
+      resolve()
+    })
+    promise.then(function () {  // 这里不能用箭头函数测试
       // @ts-ignore
-      assert(this === undefined) 
+      assert(this === undefined)
     })
   })
-  it("then 可以在同一个promise中多次调用 并且按顺序执行",(done)=>{
+  it("then 可以在同一个promise中多次调用 并且按顺序执行", (done) => {
     const callback1 = sinon.fake()
     const callback2 = sinon.fake()
     const callback3 = sinon.fake()
-    const promise = new Promise2((resolve:Function)=>{
-      resolve() 
-    })  
+    const promise = new Promise2((resolve: Function) => {
+      resolve()
+    })
     promise.then(callback1)
     promise.then(callback2)
     promise.then(callback3)
-    setTimeout(()=>{
+    setTimeout(() => {
       assert(callback1.called)
       assert(callback2.called)
       assert(callback3.called)
-      done() 
+      done()
     })
+  })
+  it("then 的返回值是一个promise", () => {
+    const promise = new Promise2((resolve: Function) => {
+      resolve()
+    })
+    const promise2 = promise.then(()=>{})
+    //@ts-ignore
+    assert(promise2 instanceof Promise2)
+  })
+  it("上一个success 的返回值传给下一个success的参数",()=>{
+
   })
 })
